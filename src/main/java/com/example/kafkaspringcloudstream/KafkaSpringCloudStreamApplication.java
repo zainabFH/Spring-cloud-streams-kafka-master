7744b3236a40867aae63f4e5ee42b3558fdcd6d6
fahmaoui zainab <zainabfahmaoui@gmail.com>
2024-01-28 21:58:24 +0100

    V1

---
+++ src/main/java/com/example/kafkaspringcloudstream/KafkaSpringCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringCloudStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudStreamApplication.class, args);
+    }
+
+}
